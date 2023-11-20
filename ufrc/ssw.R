dc52576d010cf23cc5f8aa218b0f8476432c2ffe
Dominick J. Lemas <dominicklemas@gmail.com>
2023-11-20 12:28:30 -0500

    ssw in python works

--- ufrc/ssw.R
+++ ufrc/ssw.R
@@ -4,3 +4,8 @@ cd /blue/djlemas/haswanthpopuri/Complete-Striped-Smith-Waterman-Library/
   
 
 src/ssw_test -c demo/1M.fa demo/1k.fa
+
+
+src/pyssw.py -l src/ -c /blue/djlemas/share/data/Benchmark/receptors133.fa/1cjr.recep.fa /blue/djlemas/share/data/Benchmark/ligands133.fa/1cjr.lig.fa
+
+src/ssw_test -c /blue/djlemas/share/data/Benchmark/receptors133.fa/1cjr.recep.fa /blue/djlemas/share/data/Benchmark/ligands133.fa/1cjr.lig.fa
